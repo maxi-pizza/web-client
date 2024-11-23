5ac75979ac4192a865a93e893532475017601595
INJOKER11 <67804388+INJOKER11@users.noreply.github.com>
2024-11-23 01:45:49 +0100

    Add search layout

---
+++ src/components/Search/Search.tsx
@@ -0,0 +1,42 @@
+import React from 'react';
+import SearchSvg from 'src/assets/icons/search.svg';
+import {css, useTheme} from '@emotion/react';
+import {WhiteTheme} from 'src/styles/theme.ts';
+
+const Search = () => {
+  const theme = useTheme() as WhiteTheme;
+  return (
+    <div css={container}>
+      <SearchSvg css={svgWrapper} color={theme.colors.accent} />
+      <input css={inputStyles} inputMode={'text'} placeholder={'Пошук'} />
+    </div>
+  );
+};
+
+const container = css`
+  width: 986px;
+  height: 53px;
+  position: relative;
+`;
+
+const svgWrapper = css`
+  position: absolute;
+  margin-left: 16px;
+  top: 14px;
+`;
+
+const inputStyles = theme => css`
+  padding-left: 48px;
+  width: 100%;
+  height: 100%;
+  font-family: ${theme.fontFamily};
+  color: ${theme.colors.textPrimary};
+  font-size: 16px;
+  font-weight: ${theme.fontWeights.regular};
+  border: 1px solid ${theme.colors.stroke};
+  border-radius: 8px;
+  background-color: ${theme.colors.background};
+  outline: none;
+`;
+
+export default Search;
--- src/layout/Content/Content.tsx
+++ src/layout/Content/Content.tsx
@@ -1,12 +1,16 @@
 import React from 'react';
 import {css} from '@emotion/react';
 import MenuLayout from 'src/layout/MenuLayout/MenuLayout.tsx';
+import Search from 'src/components/Search/Search.tsx';
 
 const Content = () => {
   return (
     <div css={container}>
       <div css={menuWrapper}>
         <MenuLayout />
+        <div css={searchAndProductsWrapper}>
+          <Search />
+        </div>
       </div>
     </div>
   );
@@ -16,7 +20,7 @@ const container = theme => css`
   background-color: ${theme.colors.background};
   border-top-left-radius: 80px;
   border-top-right-radius: 80px;
-  height: 800px;
+  height: 1200px;
   margin-top: -80px;
   margin-bottom: -80px;
   position: relative;
@@ -26,6 +30,11 @@ const container = theme => css`
 const menuWrapper = css`
   padding-top: 80px;
   margin-left: 135px;
+  display: flex;
+`;
+
+const searchAndProductsWrapper = css`
+  margin-left: 24px;
 `;
 
 export default Content;
