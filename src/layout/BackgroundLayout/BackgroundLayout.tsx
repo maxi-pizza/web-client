4f1a4bc21f90f816c194ad6e40190325a3d97004
INJOKER11 <67804388+INJOKER11@users.noreply.github.com>
2024-12-03 20:53:18 +0100

    Fix BackgroundLayout and BackgroundErrorLayout

--- src/layout/BackgroundLayout/BackgroundLayout.tsx
+++ src/layout/BackgroundLayout/BackgroundLayout.tsx
@@ -1,137 +1,10 @@
 import React from 'react';
-import Rectangle from 'src/assets/icons/Rectangle.svg';
-import {css, useTheme} from '@emotion/react';
-import BurritosSvg from 'src/assets/icons/burritos.svg';
-import NigiriSvg from 'src/assets/icons/nigiri.svg';
-import NoodlesSvg from 'src/assets/icons/noodles.svg';
-import FriedPotatoesSvg from 'src/assets/icons/fried-potatoes.svg';
-import PizzaSvg from 'src/assets/icons/pizza.svg';
-import RightRectangle from 'src/assets/icons/RectangleRight.svg';
-import SushiSvg from 'src/assets/icons/sushi.svg';
-import {WhiteTheme} from 'src/styles/theme.ts';
-import RectangleMobileSvg from 'src/assets/icons/rectanglemobile.svg';
+import {css} from '@emotion/react';
+import BackgroundThankYou from 'src/assets/BackgroundThankYou.png';
 
 const BackgroundLayout = ({children}: {children: React.ReactNode}) => {
-  const theme = useTheme() as WhiteTheme;
   return (
     <div css={container}>
-      <Rectangle
-        css={css`
-          display: none;
-          @media (min-width: ${theme.media.tablet}) {
-            display: block;
-            position: absolute;
-            top: 0;
-            left: 0;
-          }
-        `}
-        color={theme.colors.accent}
-      />
-      <RectangleMobileSvg
-        css={css`
-          position: absolute;
-          bottom: 0;
-          left: 0;
-          @media (min-width: ${theme.media.tablet}) {
-            display: none;
-          }
-        `}
-        color={theme.colors.accent}
-      />
-      <BurritosSvg
-        css={css`
-          display: none;
-          @media (min-width: ${theme.media.laptop}) {
-            display: block;
-            position: absolute;
-            top: 467px;
-            left: 268px;
-          }
-        `}
-        color={theme.colors.stroke}
-      />
-      <NigiriSvg
-        css={css`
-          position: absolute;
-          top: 125px;
-          right: 13px;
-          width: 60px;
-          height: 60px;
-          @media (min-width: ${theme.media.tablet}) {
-            right: 200px;
-            top: 176px;
-            width: 120px;
-            height: 120px;
-          }
-        `}
-        color={theme.colors.stroke}
-      />
-      <PizzaSvg
-        css={css`
-          position: absolute;
-          top: 101px;
-          left: 40px;
-          width: 60px;
-          height: 60px;
-          @media (min-width: ${theme.media.tablet}) {
-            top: unset;
-            bottom: 150px;
-            right: 336px;
-            left: unset;
-            width: 120px;
-            height: 120px;
-          }
-        `}
-        color={theme.colors.stroke}
-      />
-      <NoodlesSvg
-        css={css`
-          display: none;
-          @media (min-width: ${theme.media.laptop}) {
-            display: block;
-            position: absolute;
-            top: 216px;
-            left: 315px;
-          }
-        `}
-        color={theme.colors.stroke}
-      />
-      <FriedPotatoesSvg
-        css={css`
-          display: none;
-          @media (min-width: ${theme.media.laptop}) {
-            display: block;
-            position: absolute;
-            top: 367px;
-            right: 484px;
-          }
-        `}
-        color={theme.colors.stroke}
-      />
-      <RightRectangle
-        css={css`
-          display: none;
-          @media (min-width: ${theme.media.tablet}) {
-            display: block;
-            position: absolute;
-            right: 0;
-            bottom: 0;
-          }
-        `}
-        color={theme.colors.accent}
-      />
-      <SushiSvg
-        css={css`
-          display: none;
-          @media (min-width: ${theme.media.laptop}) {
-            display: block;
-            position: absolute;
-            bottom: 137px;
-            left: 343px;
-          }
-        `}
-        color={theme.colors.stroke}
-      />
       <div>{children}</div>
     </div>
   );
@@ -139,6 +12,10 @@ const BackgroundLayout = ({children}: {children: React.ReactNode}) => {
 
 const container = theme => css`
   background-color: ${theme.colors.background};
+  background-image: url(${BackgroundThankYou});
+  background-size: cover;
+  background-position: center;
+  background-repeat: no-repeat;
   height: 100vh;
   margin-top: -92px;
   position: relative;
--- src/layout/ErrorBackgroundLayout/ErrorBackgroundLayout.tsx
+++ src/layout/ErrorBackgroundLayout/ErrorBackgroundLayout.tsx
@@ -1,126 +1,11 @@
 import React from 'react';
-import {css, useTheme} from '@emotion/react';
-import {WhiteTheme} from 'src/styles/theme.ts';
-import Rectangle from 'src/assets/icons/Rectangle.svg';
-import RectangleMobileSvg from 'src/assets/icons/rectanglemobile.svg';
-import NigiriSvg from 'src/assets/icons/nigiri.svg';
-import NoodlesSvg from 'src/assets/icons/noodles.svg';
-import FriedPotatoesSvg from 'src/assets/icons/fried-potatoes.svg';
-import PizzaSvg from 'src/assets/icons/pizza.svg';
-import RightRectangle from 'src/assets/icons/RectangleRight.svg';
-import SodaSvg from 'src/assets/icons/soda.svg';
+import {css} from '@emotion/react';
+import BackgroundError from 'src/assets/BackgroundError.png';
+import BackgroundErrorMobile from 'src/assets/BackgroundErrorMobile.png';
 
 const ErrorBackgroundLayout = ({children}: {children: React.ReactNode}) => {
-  const theme = useTheme() as WhiteTheme;
   return (
     <div css={container}>
-      <Rectangle
-        css={css`
-          display: none;
-          @media (min-width: ${theme.media.tablet}) {
-            display: flex;
-            transform: rotate(90deg);
-            position: absolute;
-            top: 0;
-            right: 0;
-          }
-        `}
-        color={theme.colors.accent}
-      />
-      <RectangleMobileSvg
-        css={css`
-          position: absolute;
-          bottom: 0;
-          left: 0;
-          @media (min-width: ${theme.media.tablet}) {
-            display: none;
-          }
-        `}
-      />
-      <SodaSvg
-        css={css`
-          display: none;
-          @media (min-width: ${theme.media.tablet}) {
-            display: block;
-            position: absolute;
-            top: 433px;
-            left: 524px;
-          }
-        `}
-      />
-      <NigiriSvg
-        css={css`
-          position: absolute;
-          top: 336px;
-          right: 43px;
-          width: 60px;
-          height: 60px;
-          @media (min-width: ${theme.media.tablet}) {
-            right: 328px;
-            top: 188px;
-            width: 120px;
-            height: 120px;
-          }
-        `}
-        color={theme.colors.stroke}
-      />
-      <NoodlesSvg
-        css={css`
-          position: absolute;
-          top: 340px;
-          left: 35px;
-          width: 60px;
-          height: 60px;
-          @media (min-width: ${theme.media.tablet}) {
-            top: 186px;
-            left: 600px;
-            width: 120px;
-            height: 120px;
-          }
-        `}
-        color={theme.colors.stroke}
-      />
-      <FriedPotatoesSvg
-        css={css`
-          position: absolute;
-          top: 430px;
-          right: 148px;
-          width: 60px;
-          height: 60px;
-          @media (min-width: ${theme.media.tablet}) {
-            top: 300px;
-            right: 637px;
-            width: 120px;
-            height: 120px;
-          }
-        `}
-        color={theme.colors.stroke}
-      />
-      <PizzaSvg
-        css={css`
-          display: none;
-          @media (min-width: ${theme.media.tablet}) {
-            display: block;
-            position: absolute;
-            bottom: 150px;
-            left: 317px;
-          }
-        `}
-        color={theme.colors.stroke}
-      />
-      <RightRectangle
-        css={css`
-          display: none;
-          @media (min-width: ${theme.media.tablet}) {
-            display: block;
-            position: absolute;
-            left: 0;
-            bottom: 0;
-            transform: rotate(90deg);
-          }
-        `}
-        color={theme.colors.accent}
-      />
       <div>{children}</div>
     </div>
   );
@@ -128,6 +13,10 @@ const ErrorBackgroundLayout = ({children}: {children: React.ReactNode}) => {
 
 const container = theme => css`
   background-color: ${theme.colors.background};
+  background-image: url(${BackgroundErrorMobile});
+  background-size: cover;
+  background-position: center;
+  background-repeat: no-repeat;
   height: 100vh;
   margin-top: -92px;
   position: relative;
@@ -135,7 +24,9 @@ const container = theme => css`
   justify-content: center;
   align-items: center;
   @media (min-width: ${theme.media.tablet}) {
+    background-image: url(${BackgroundError});
     justify-content: flex-start;
+    width: 100%;
   }
 `;
 
